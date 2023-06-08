2eceaad8aa0d8790f504874034f36a52dce5f80c
陳建成（Chien-Cheng (Jeff) Chen） <43311603+jeffeuxMartin@users.noreply.github.com>
2023-06-08 08:48:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+import os
+
 import requests
 from flask import Flask, redirect
 from urllib.parse import urlparse, parse_qs
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "Hello, World!"
-
+   print('Request for index page received')
+   return render_template('index.html')
 
 @app.route('/<video_ids>')
 def get_playlist_page(video_ids):
@@ -24,7 +29,24 @@ def get_playlist_page(video_ids):
     else:
         return "No playlist ID found."
 
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# 
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#    name = request.form.get('name')
+# 
+#    if name:
+#        print('Request for hello page received with name=%s' % name)
+#        return render_template('hello.html', name = name)
+#    else:
+#        print('Request for hello page received with no name or blank name -- redirecting')
+#        return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-    app.run()
+   app.run()
 
 # http://127.0.0.1:5000/uVWJoU1Yv1U,aTvtb6syC9Q,khwlvvKc8Dk
